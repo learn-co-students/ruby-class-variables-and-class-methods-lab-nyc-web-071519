4db6afd36280c6ef26c0ec07ffd790acb02c4c8c
Tyler Knapp <tgknapp11@gmail.com>
2019-07-18 22:15:44 -0400

    Done.

--- lib/run.rb
+++ lib/run.rb
@@ -2,9 +2,9 @@ require_relative 'song.rb'
 require 'pry'
 
 
-Song.new({name: "Holiday in Cambodia", artist: "Dead Kennedys", genre: "Rock"})
+Song.new({name: "We Were Young", artist: "Petit Biscuit", genre: "Electronic"})
 Song.new({name: "Moon Over Marin", artist: "Dead Kennedys", genre: "Rock"})
-Song.new({name: "Seek & Destroy", artist: "Metallica", genre: "Metal"})
+Song.new({name: "Talk to Me You'll Understand", artist: "Ross from Friends", genre: "Electronic"})
 
 binding.pry
 puts "done"
